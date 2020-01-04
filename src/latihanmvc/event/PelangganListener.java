c05a938377a2717cb938d9163a576579524fa10f
Agus Awaludin <55693286+AgusAwaludinUnikom@users.noreply.github.com>
2020-01-04 20:26:31 +0700

    Add files via upload

---
+++ src/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanmvc.event;
+
+import latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author 
+ * NAMA     : Agus Awaludin
+ * NIM      : 10118067
+ * KELAS    : PBO2
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+        
+    
+}
---
+++ src/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanmvc.model;
+
+import latihanmvc.view.PelangganView;
+
+/**
+ *
+ * @author 
+ * NAMA     : Agus Awaludin
+ * NIM      : 10118067
+ * KELAS    : PBO2
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PelangganView pelanggan = new PelangganView();
+        pelanggan.setVisible(true);
+        
+    }
+    
+}
